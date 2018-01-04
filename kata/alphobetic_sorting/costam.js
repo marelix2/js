6d5dfdaaaef8268c39a5d7f459686af1fd999872
marelix2 <marelix2@gmail.com>
2018-01-04 17:34:19 +0100

    stuff

---
+++ kata/alphobetic_sorting/costam.js
@@ -0,0 +1,7 @@
+  function transform52 (){
+
+      var str = document.getElementById("string52").value,
+      toArray = str.split("").sort();
+
+         document.getElementById("result52").innerHTML = toArray.join("");
+}
